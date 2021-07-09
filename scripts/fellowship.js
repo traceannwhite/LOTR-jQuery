84f532d4c129b51d6318494e2d0f8d1342d31063
traceannwhite <traceannwhite@gmail.com>
2021-07-08 21:45:56 -0500

    Chapter 10 complete - hord of gondor blew and Boromir is dead

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -193,6 +193,9 @@ const hornOfGondor = () => {
   // 1. create a pop-up alert that the horn of gondor has been blown
   // 2. Boromir's been killed by the Uruk-hai! Put a linethrough on Boromir's name
   // 3. Tricky: Remove the Uruk-Hai from the Baddies on the page
+  alert("The horn of Gondor has been blown!");
+  $(".buddy").eq(4).css("text-decoration", "line-through");
+  $(".baddy").eq(2).remove();
 };
 
 // COMMIT YOUR WORK
